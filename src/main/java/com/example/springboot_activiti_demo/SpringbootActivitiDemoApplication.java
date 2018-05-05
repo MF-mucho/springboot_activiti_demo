97a06d03a7a9d0b717a08687b130b050e6dbd08b
mucho <865089319@qq.com>
2018-05-05 18:52:37 +0800

    springboot 集成 avtiviti 的 demo

---
+++ src/main/java/com/example/springboot_activiti_demo/SpringbootActivitiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot_activiti_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActivitiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootActivitiDemoApplication.class, args);
+	}
+}
